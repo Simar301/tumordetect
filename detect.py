1989c2252aab288cfdda74ca602dfacc4da1802e
simar <ajargaming847@gmail.com>
2023-11-27 12:48:58 +0530

    done

--- detect.py
+++ detect.py
@@ -49,12 +49,10 @@ model.add(Conv2D(32,(3,3),input_shape=(64,64,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model=Sequential()
 model.add(Conv2D(32,(3,3),kernel_initializer='he_uniform'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
-model=Sequential()
 model.add(Conv2D(32,(3,3),kernel_initializer='he_uniform'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
